980ab4cd94652a3ba953f9845418c88cde3b82fa
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-07-01 02:16:23 +0300

    0.0.1

---
+++ 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _01._07._21_EXAM_Internet_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
