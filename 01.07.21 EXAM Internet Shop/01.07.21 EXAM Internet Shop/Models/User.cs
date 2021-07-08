2c5390a9d25e16dda11e443d76444a753ba65cb6
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-07-08 23:45:15 +0300

    v 0.0.2

---
+++ 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01._07._21_EXAM_Internet_Shop.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Column("Id")]  // Можно было не указывать потому, что так было бы по умолчанию, благодаря соглашению о наименованиях EF
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Enter an username")]
+        [DataType(DataType.Text)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Incorrect length")]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Enter a password")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Incorrect length")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Password Confirmation")]
+        //[Required(ErrorMessage = "Incorrect password")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Incorrect length")]
+        [Compare("Password", ErrorMessage = "Passwords are not equal")]
+        public string PasswordConfirmation { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Enter an email")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(320, ErrorMessage = "Incorrect length")]
+        public string Email { get; set; }
+    }
+}
