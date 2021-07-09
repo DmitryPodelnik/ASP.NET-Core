030bc61c5ae7030efa8d1613687dcfd86108edf7
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-07-10 02:32:47 +0300

    v 0.1.3

---
+++ 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Models/Database/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01._07._21_EXAM_Internet_Shop.Models.Database.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+              new Product[]
+              {
+
+              });
+        }
+    }
+}
