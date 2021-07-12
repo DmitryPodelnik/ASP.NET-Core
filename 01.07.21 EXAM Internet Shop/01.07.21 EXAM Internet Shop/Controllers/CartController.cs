be0db6b79e4950b16e1f881722e93f71186343e5
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-07-12 16:19:26 +0300

    v 1.0.9

--- 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Controllers/CartController.cs
+++ 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _01._07._21_EXAM_Online_Store;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,14 @@ namespace _01._07._21_EXAM_Internet_Shop.Controllers
 {
     public class CartController : Controller
     {
+        private readonly OnlineStoreDbContext _context;
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+
     }
 }
--- 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Models/Cart.cs
+++ 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Models/Cart.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace _01._07._21_EXAM_Internet_Shop.Models
 {
+    [Table("Carts")]
     public class Cart
     {
+        [Column("Id")]  // Можно было не указывать потому, что так было бы по умолчанию, благодаря соглашению о наименованиях EF
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        public List<Product> Products { get; set; } = new();
 
     }
 }
