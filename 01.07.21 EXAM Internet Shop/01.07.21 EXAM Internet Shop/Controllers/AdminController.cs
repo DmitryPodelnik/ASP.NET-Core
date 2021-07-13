e64f6eafb7dbd5cac9b50d4c8d12c3a40bab1eca
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-07-13 16:13:00 +0300

    v 1.2.4

--- 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Controllers/AdminController.cs
+++ 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using _01._07._21_EXAM_Online_Store;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,23 @@ namespace _01._07._21_EXAM_Internet_Shop.Controllers
     [Route("admin")]
     public class AdminController : Controller
     {
+        private readonly OnlineStoreDbContext _context;
+
+        public AdminController(OnlineStoreDbContext context)
+        {
+            _context = context;
+        }
+
         [Authorize(Roles = "admin")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            return View(await _context.Categories.ToListAsync());
         }
     }
 }
