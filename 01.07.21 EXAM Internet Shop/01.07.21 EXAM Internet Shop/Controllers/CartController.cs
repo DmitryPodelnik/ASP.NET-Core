b42941548ce7a17a530cd5e53731747b75a53c54
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-07-16 03:49:11 +0300

    v 1.5.1

--- 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Controllers/CartController.cs
+++ 01.07.21 EXAM Internet Shop/01.07.21 EXAM Internet Shop/Controllers/CartController.cs
@@ -33,9 +33,10 @@ namespace _01._07._21_EXAM_Internet_Shop.Controllers
         {
             var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
             var currentUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == HttpContext.User.Identity.Name);
+            var existedCart = await _context.Carts.FirstOrDefaultAsync(c => c.UserId == currentUser.Id);
             //var guid = Guid.NewGuid().ToString();
 
-            if (currentUser.Cart == null)
+            if (existedCart == null)
             {
                 Cart cart = new();
                 cart.User = currentUser;
@@ -43,17 +44,13 @@ namespace _01._07._21_EXAM_Internet_Shop.Controllers
                 _context.SaveChanges();
 
                 cart.Products.Add(product);
-                _context.SaveChanges();
             }
             else
             {
-
+                currentUser.Cart.Products.Add(product);
             }
 
-            //var cart = await _context.Carts.Include(c => c.Products).FirstOrDefaultAsync(c => c.UserId == currentUser.Id);
-            //cart.Products.Add(product);
-
-            //await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
             return RedirectToAction("GetProducts", "Cart");
         }
